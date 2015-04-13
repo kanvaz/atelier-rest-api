abc123856f32cb531e0cdd3d885c218464b0dee0
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2015-04-13 23:01:27 +0200

    feat(*): first runnable version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+#[macro_use]
+extern crate nickel;
+extern crate git_grid;
+
+use nickel::Nickel;
+use nickel::router::http_router::HttpRouter;
+use nickel::JsonBody;
+use git_grid::file_set::FileSet;
+use git_grid::repository_locator::{ self, RepositoryState };
+
 fn main() {
-    println!("Hello, world!");
+    let mut server = Nickel::new();
+    let mut router = Nickel::router();
+
+    //curl 'http://localhost:6767/kanvaz' -H 'Content-Type: application/json;charset=UTF-8'  --data-binary $'{ "files": [{ "name":"style.css", "content": "button: { color: red; }"}] }'
+    router.post("/kanvaz", middleware! { |request, response|
+        let file_set = request.json_as::<FileSet>().unwrap();
+        let repository = repository_locator::get_repository_handle(RepositoryState::NonExisting);
+        repository.add_files_and_commit(file_set.files, "SAVEPOINT");
+        format!("{:?}", repository)
+    });
+
+    //curl 'http://localhost:6767/kanvaz/<PUT-REPOSITORY-ID-HERE>'
+    router.get("/kanvaz/:id", middleware! { |request, response|
+        let repository = repository_locator::get_repository_handle(RepositoryState::Existing(request.param("id")));
+        let file_set = FileSet { files: repository.read_all_files() };
+        format!("{}", file_set.to_pretty_json())
+    });
+
+    server.utilize(router);
+
+    server.listen("127.0.0.1:6767");
 }
